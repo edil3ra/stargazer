8c842e54ada926356bd4524ac52e7580dd77af70
edil3ra <edil3ra@gmail.com>
2021-12-30 16:16:52 +0100

    :sparkles: :bug: :construction: add token auth middlweare, fix serialization

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-import json
+from serde.json import to_json
 
 import falcon
 
@@ -14,13 +14,33 @@ class StarNeighbours:
         r = models.RepoStargazer('my-repo', [u1, u2, u3])
 
         print('user: {}'.format(user))
-        res.text = json.dumps(r)
+        res.text = to_json(r)
 
+
+class AuthMiddleware:
+    def process_request(self, req, resp):
+        token = req.get_header('Authorization')
+
+        if token is None:
+            description = ('Please provide an auth token '
+                           'as part of the request.')
+
+            raise falcon.HTTPUnauthorized(title='Auth token required', description=description)
+
+        if not self._token_is_valid(token):
+            description = ('The provided auth token is not valid. '
+                           'Please request a new token and try again.')
+
+            raise falcon.HTTPUnauthorized(title='Authentication required', description=description)
+
+    def _token_is_valid(self, token):
+        return token == 'token'
 
 
 def build_app():
+    # app = falcon.App(middleware=[AuthMiddleware()])
     app = falcon.App()
-    things = StarNeighbours()
-    app.add_route('/repos/{user}/starneighbours', things)
+    stars = StarNeighbours()
+    app.add_route('/repos/{user}/starneighbours', stars)
     return app
     
